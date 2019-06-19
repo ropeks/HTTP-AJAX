ce8108ddad58d2d6c14c3c88f01853824a6da6d3
ropeks <ropeksmilioner@gmail.com>
2019-06-19 14:18:04 +0200

    flesh out components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import FriendsList from './components/FriendsList';
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsData: null
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FriendsList />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class FriendsList extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                friends list
+            </div>
+        );
+    }
+} 
